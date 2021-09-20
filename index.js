b166c0d5903a354c69c704b23ca5d990b65f191d
m0hammadr3za <mohammad.reza.gholami.dev@gmail.com>
2021-09-21 01:33:01 +0430

    Corrected the react app connection path

--- index.js
+++ index.js
@@ -25,9 +25,9 @@ app.use(express.static("public"));
 app.use("/api/articles", articles);
 
 // Connect the react app
-app.use(express.static(path.resolve(__dirname, "../client/build")));
+app.use(express.static(path.resolve(__dirname, "client", "build")));
 app.get("*", (req, res) => {
-    return res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+    return res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 
 // Error handler
