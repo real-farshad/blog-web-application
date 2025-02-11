5dc39313d99f6c0e408f99693776df7e5c66bd92
Farshad <m0hammadr3za.dev@gmail.com>
2025-02-11 06:36:40 +0330

    📦 update packages

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,46 @@
 import { useState } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoadingScreen from "./components/LoadingScreen";
 import Article from "./pages/Article";
 import "./styles/style.scss";
 
 export default function App() {
-    const [article, setArticle] = useState(null);
+  const [article, setArticle] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [closeLoadingScreen, setCloseLoadingScreen] = useState(false);
 
-    const [loading, setLoading] = useState(false);
-    const [closeLoadingScreen, setCloseLoadingScreen] = useState(false);
+  async function getArticle(id) {
+    setLoading(true);
+    window.scrollTo(0, 0);
 
-    async function getArticle(id) {
-        setLoading(true);
+    const url = id ? `/api/articles/${id}` : "/api/articles/first-article";
+    const res = await fetch(url);
+    const newArticle = await res.json();
 
-        window.scrollTo(0, 0);
+    setArticle(newArticle);
+    setCloseLoadingScreen(true);
 
-        const url = id ? `/api/articles/${id}` : "/api/articles/first-article";
+    setTimeout(() => {
+      setLoading(false);
+      setCloseLoadingScreen(false);
+    }, 1000);
+  }
 
-        const res = await fetch(url);
-        const newArticle = await res.json();
-
-        setArticle(newArticle);
-        setCloseLoadingScreen(true);
-
-        const finishLoadingTimer = setTimeout(() => {
-            setLoading(false);
-            setCloseLoadingScreen(false);
-            clearTimeout(finishLoadingTimer);
-        }, 1000);
-    }
-
-    return (
-        <>
-            {loading && <LoadingScreen closeLoadingScreen={closeLoadingScreen} />}
-            <Router>
-                <Switch>
-                    <Route exact path="/:articleId">
-                        <Article article={article} getArticle={getArticle} />
-                    </Route>
-                    <Route path="/">
-                        <Article article={article} getArticle={getArticle} />
-                    </Route>
-                </Switch>
-            </Router>
-        </>
-    );
+  return (
+    <>
+      {loading && <LoadingScreen closeLoadingScreen={closeLoadingScreen} />}
+      <Router>
+        <Routes>
+          <Route
+            path="/:articleId"
+            element={<Article article={article} getArticle={getArticle} />}
+          />
+          <Route
+            path="/"
+            element={<Article article={article} getArticle={getArticle} />}
+          />
+        </Routes>
+      </Router>
+    </>
+  );
 }
